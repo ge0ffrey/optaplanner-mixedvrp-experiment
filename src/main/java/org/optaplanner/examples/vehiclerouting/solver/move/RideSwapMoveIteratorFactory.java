672177f060e7b774b1abedaa2171417ad3832767
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-10-11 14:09:24 +0200

    RideSwapMoveIteratorFactory

---
+++ src/main/java/org/optaplanner/examples/vehiclerouting/solver/move/RideSwapMoveIteratorFactory.java
@@ -0,0 +1,93 @@
+package org.optaplanner.examples.vehiclerouting.solver.move;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.optaplanner.core.impl.domain.variable.descriptor.GenuineVariableDescriptor;
+import org.optaplanner.core.impl.domain.variable.inverserelation.SingletonInverseVariableDemand;
+import org.optaplanner.core.impl.domain.variable.inverserelation.SingletonInverseVariableSupply;
+import org.optaplanner.core.impl.heuristic.move.CompositeMove;
+import org.optaplanner.core.impl.heuristic.move.Move;
+import org.optaplanner.core.impl.heuristic.selector.move.factory.MoveIteratorFactory;
+import org.optaplanner.core.impl.heuristic.selector.move.generic.chained.ChainedSwapMove;
+import org.optaplanner.core.impl.score.director.InnerScoreDirector;
+import org.optaplanner.core.impl.score.director.ScoreDirector;
+import org.optaplanner.examples.vehiclerouting.domain.Ride;
+import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
+import org.optaplanner.examples.vehiclerouting.domain.Visit;
+
+public class RideSwapMoveIteratorFactory implements MoveIteratorFactory<VehicleRoutingSolution> {
+
+    private List<GenuineVariableDescriptor<VehicleRoutingSolution>> variableDescriptorList = null;
+    private List<SingletonInverseVariableSupply> inverseVariableSupplyList = null;
+
+    @Override
+    public long getSize(ScoreDirector<VehicleRoutingSolution> scoreDirector) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<? extends Move<VehicleRoutingSolution>> createOriginalMoveIterator(ScoreDirector<VehicleRoutingSolution> scoreDirector) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<? extends Move<VehicleRoutingSolution>> createRandomMoveIterator(
+            ScoreDirector<VehicleRoutingSolution> scoreDirector, Random workingRandom) {
+        // TODO DO NOT USE InnerScoreDirector! This is an experiment, don't put this in production!
+        InnerScoreDirector<VehicleRoutingSolution> innerScoreDirector = (InnerScoreDirector<VehicleRoutingSolution>) scoreDirector;
+        GenuineVariableDescriptor<VehicleRoutingSolution> variableDescriptor
+                = innerScoreDirector.getSolutionDescriptor().findEntityDescriptorOrFail(Visit.class)
+                .getGenuineVariableDescriptor("previousStandstill");
+        // TODO DIRTY HACK due to lack of lifecycle methods
+        if (inverseVariableSupplyList == null) {
+            variableDescriptorList = Collections.singletonList(variableDescriptor);
+            inverseVariableSupplyList = Collections.singletonList(innerScoreDirector.getSupplyManager()
+                    .demand(new SingletonInverseVariableDemand(variableDescriptor)));
+        }
+
+        VehicleRoutingSolution solution = scoreDirector.getWorkingSolution();
+        List<Ride> rideList = solution.getRideList();
+        return new RideSwapMoveIterator(rideList, workingRandom);
+    }
+
+    private class RideSwapMoveIterator implements Iterator<Move<VehicleRoutingSolution>> {
+
+        private final List<Ride> rideList;
+        private final Random workingRandom;
+
+        public RideSwapMoveIterator(List<Ride> rideList, Random workingRandom) {
+            this.rideList = rideList;
+            this.workingRandom = workingRandom;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return rideList.size() >= 2;
+        }
+
+        @Override
+        public Move<VehicleRoutingSolution> next() {
+            int leftRideIndex = workingRandom.nextInt(rideList.size());
+            Ride leftRide = rideList.get(leftRideIndex);
+            int rightRideIndex = workingRandom.nextInt(rideList.size() - 1);
+            if (rightRideIndex >= leftRideIndex) {
+                rightRideIndex++;
+            }
+            Ride rightRide = rideList.get(rightRideIndex);
+
+            return new CompositeMove<VehicleRoutingSolution>(
+                    new ChainedSwapMove<VehicleRoutingSolution>(variableDescriptorList, inverseVariableSupplyList,
+                            leftRide.getPickupVisit(), rightRide.getPickupVisit()),
+                    new ChainedSwapMove<VehicleRoutingSolution>(variableDescriptorList, inverseVariableSupplyList,
+                            leftRide.getDeliveryVisit(), rightRide.getDeliveryVisit())
+            );
+
+        }
+
+    }
+
+}
